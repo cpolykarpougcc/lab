be316208fdd46e3cff8cef25a0ee02abe05ecbe1
Charis Polykarpou <charis.polykarpou@gcc.com.cy>
2026-01-07 12:46:33 +0200

    revert back

--- vsphere.tf
+++ vsphere.tf
@@ -16,42 +16,30 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-# Datacenter
 data "vsphere_datacenter" "dc" {
   name = "LAB Datacenter"
 }
 
-# Compute Cluster (optional, useful if you want to reference cluster-specific pool)
-data "vsphere_compute_cluster" "compute_cluster" {
-  name          = "Your-Cluster-Name" # replace with your cluster name
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Resource Pool (fixed)
 data "vsphere_resource_pool" "pool" {
   name          = "Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  resource_pool_id = data.vsphere_compute_cluster.compute_cluster.resource_pool_id
 }
 
-# Datastore
 data "vsphere_datastore" "datastore" {
   name          = "DS3"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Network
 data "vsphere_network" "network" {
   name          = "DEV_LAB VM Network"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Template VM to clone
 data "vsphere_virtual_machine" "ubuntu20_04" {
   name          = "ubuntu20-04-temp"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# New VM resource
 resource "vsphere_virtual_machine" "ubu_testing" {
   name     = "ubu-test"
   num_cpus = 2
@@ -92,7 +80,6 @@ resource "vsphere_virtual_machine" "ubu_testing" {
   }
 }
 
-# Outputs
 output "VM_Name" {
   value = vsphere_virtual_machine.ubu_testing.name
 }
@@ -100,3 +87,4 @@ output "VM_Name" {
 output "VM_IP_Address" {
   value = vsphere_virtual_machine.ubu_testing.guest_ip_addresses
 }
+
