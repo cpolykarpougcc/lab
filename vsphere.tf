6d165e34558304f2cd56a13d5e4ddf423a794a43
Charis Polykarpou <charis.polykarpou@gcc.com.cy>
2026-01-08 09:37:17 +0200

    change folder stracture

--- vsphere.tf
+++ vsphere.tf
@@ -32,10 +32,10 @@ data "vsphere_compute_cluster" "cluster" {
 }
 
 # ------------------------
-# VM Folder (ABSOLUTE PATH)
+# VM Folder (ABSOLUTE PATH – REQUIRED)
 # ------------------------
 data "vsphere_folder" "vm_folder" {
-  path = "/${data.vsphere_datacenter.datacenter.name}/vm/${var.vsphere_folder}"
+  path = "LAB Datacenter/vm/SYSENG/Charis"
 }
 
 # ------------------------
@@ -67,7 +67,7 @@ data "vsphere_virtual_machine" "template" {
 # ------------------------
 resource "vsphere_virtual_machine" "ubu_testing" {
   name             = "ubu-test"
-  folder           = trimprefix(data.vsphere_folder.folder.path, "/${data.vsphere_datacenter.datacenter.name}/vm")
+  folder           = data.vsphere_folder.vm_folder.path
   num_cpus         = 2
   memory           = 4096
 
