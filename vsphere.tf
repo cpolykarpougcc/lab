7a90ed02d95fe8881a61b38dcdae7454c9002b60
Charis Polykarpou <charis.polykarpou@gcc.com.cy>
2026-01-08 10:13:58 +0200

    fix disk block

--- vsphere.tf
+++ vsphere.tf
@@ -67,6 +67,7 @@ resource "vsphere_virtual_machine" "ubu_testing" {
 
   guest_id  = data.vsphere_virtual_machine.template.guest_id
   scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  firmware  = data.vsphere_virtual_machine.template.firmware
 
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
@@ -76,7 +77,12 @@ resource "vsphere_virtual_machine" "ubu_testing" {
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
-  # ❗ DO NOT DEFINE disk {} WHEN CLONING — avoids "OS not found"
+  # REQUIRED disk block (do NOT change layout)
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks[0].size
+    thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
+  }
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
